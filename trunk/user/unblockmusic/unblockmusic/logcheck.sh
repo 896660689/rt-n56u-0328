e1c05d28fc2a610f07fb5c094918911a09e6d8ba
896660689 <896660689@qq.com>
2020-07-22 18:01:32 +0800

    Update logcheck.sh

--- trunk/user/unblockmusic/unblockmusic/logcheck.sh
+++ trunk/user/unblockmusic/unblockmusic/logcheck.sh
@@ -4,14 +4,14 @@ log_max_size=100
 log_file="/tmp/unblockmusic.log"
 log_size=0
 
-/usr/bin/getmusicip.sh
+/etc/storage/unblockmusic/getmusicip.sh
 sleep 29s
 
 while true
 do
     icount=`busybox ps -w | grep UnblockNeteaseMusic | grep -v grep | grep -v logcheck.sh`
     if [ -z "$icount" ]; then
-        /usr/bin/getmusicip.sh
+        /etc/storage/unblockmusic/getmusicip.sh
         /usr/bin/unblockmusic restart
     fi
     log_size=$(expr $(ls -l $log_file | awk '{print $5}') / 1024)
